d5d788829a46c4d6d1046d0bde5f15030cc829ac
JohnSloan8 <johnsloan1984@gmail.com>
2022-07-25 14:41:50 +0100

    removed aws

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,30 +9,4 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "clcsawsbucket.s3.eu-west-1.amazonaws.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "clcsawsbucket.s3.eu-west-1.amazonaws.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
 ];
